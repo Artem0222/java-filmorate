d521368c1c39cd559b40451211cea07b8ef57f9c
Artem0222 <arzerby11@github.com>
2026-06-08 23:39:24 +0300

    лишние импорты

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,22 +1,17 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
-import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import lombok.extern.slf4j.Slf4j;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
-import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.time.LocalDate;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RequestMapping("/films")
@@ -68,34 +63,35 @@ public class FilmController {
     public void addLike(@PathVariable Long id, @PathVariable Long userId) {
         log.info("Пользователь {} ставит лайк фильму {}", userId, id);
         filmService.addLike(id, userId);
-}
-@DeleteMapping("/{id}/like/{userId}")
-public void removeLike(@PathVariable Long id, @PathVariable Long userId) {
-    log.info("Пользователь {} удаляет лайк у фильма {}", userId, id);
-    filmService.removeLike(id, userId);
-}
+    }
 
-@GetMapping("/popular")
-public List<Film> getPopular(@RequestParam(required = false) Integer count) {
-    log.info("Запрос популярных фильмов, count={}", count);
-   return filmService.getPopularFilms(count);
-}
+    @DeleteMapping("/{id}/like/{userId}")
+    public void removeLike(@PathVariable Long id, @PathVariable Long userId) {
+        log.info("Пользователь {} удаляет лайк у фильма {}", userId, id);
+        filmService.removeLike(id, userId);
+    }
 
-private void validateFilm(Film film) {
-    if (film.getName() == null || film.getName().isBlank()) {
-        throw new ValidationException("Название фильма не может быть пустым");
+    @GetMapping("/popular")
+    public List<Film> getPopular(@RequestParam(required = false) Integer count) {
+        log.info("Запрос популярных фильмов, count={}", count);
+        return filmService.getPopularFilms(count);
     }
-    if (film.getDescription() != null && film.getDescription().length() > 200) {
-        throw new ValidationException("Максимальная длина описания — 200 символов");
+
+    private void validateFilm(Film film) {
+        if (film.getName() == null || film.getName().isBlank()) {
+            throw new ValidationException("Название фильма не может быть пустым");
+        }
+        if (film.getDescription() != null && film.getDescription().length() > 200) {
+            throw new ValidationException("Максимальная длина описания — 200 символов");
+        }
+        if (film.getReleaseDate() == null) {
+            throw new ValidationException("Дата релиза должна быть указана");
+        }
+        if (film.getReleaseDate().isBefore(FIRST_FILM_DATE)) {
+            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года");
+        }
+        if (film.getDuration() == null || film.getDuration() <= 0) {
+            throw new ValidationException("Продолжительность фильма должна быть положительным числом");
+        }
     }
-    if (film.getReleaseDate() == null) {
-        throw new ValidationException("Дата релиза должна быть указана");
-    }
-    if (film.getReleaseDate().isBefore(FIRST_FILM_DATE)) {
-        throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года");
-    }
-    if (film.getDuration() == null || film.getDuration() <= 0) {
-        throw new ValidationException("Продолжительность фильма должна быть положительным числом");
-    }
-}
 }
