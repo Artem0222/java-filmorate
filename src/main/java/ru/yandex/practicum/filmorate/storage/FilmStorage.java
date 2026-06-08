af6c12c791398fdc55ccbf9b87c506b8ff76ece6
Artem0222 <arzerby11@github.com>
2026-06-08 23:42:46 +0300

    checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -1,15 +1,21 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.Film;
+
 import java.util.Collection;
 import java.util.Optional;
 
 
 public interface FilmStorage {
     Collection<Film> findAll();
+
     Optional<Film> findById(Long id);
+
     Film save(Film film);
+
     Film update(Film film);
+
     void deleteById(Long id);
+
     boolean existsById(Long id);
 }
